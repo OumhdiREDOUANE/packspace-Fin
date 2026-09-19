3c2e4578bcd5f4c2127035c6c0814f1edc4ac63c
OumhdiREDOUANE <redouaneoumhdi@gmail.coM>
2026-09-19 17:37:50 +0100

    loading deployement

--- packspaceui/src/app/(site)/cartClient/cartClient.jsx
+++ packspaceui/src/app/(site)/cartClient/cartClient.jsx
@@ -6,10 +6,21 @@ import useSWR, { mutate } from "swr";
 import { useRouter } from "next/navigation";
 import { useAuth } from "src/app/components/AuthProvider";
 import toast from 'react-hot-toast';
-const fetcher = (url, token) =>
-  fetch(url, {
-    headers: token ? { "Authorization": `Bearer ${token}` } : {},
-  }).then(res => res.json());
+const fetcher = async (url, token) => {
+  const res = await fetch(url, {
+    headers: token
+      ? {
+          Authorization: `Bearer ${token}`,
+        }
+      : {},
+  });
+
+  if (!res.ok) {
+    throw new Error("Cart API failed");
+  }
+
+  return res.json();
+};
 
 export default function CartClient({ sessionId, orders, token }) {
   const { isLoggedIn } = useAuth();
@@ -32,19 +43,23 @@ const isDisabledActions = isLoggedIn && isPanierEmpty;
       : `${API_BASE_URL}/api/cart/notLogin/${sessionId}`
     : null;
 
-  const { data } = useSWR(url, (url) => fetcher(url, token), {
-    fallbackData: orders,
+ const { data, error } = useSWR(
+  url,
+  (url) => fetcher(url, token),
+  {
     refreshInterval: 5000,
-  });
+    shouldRetryOnError: true,
+    errorRetryInterval: 1000,
+    errorRetryCount: Infinity,
+  }
+);
 
-  useEffect(() => {
-    if (data) {
-      setDataOfOrders(data);
-     
-      
-      setLoading(false);
-    }
-  }, [data]);
+ useEffect(() => {
+  if (data) {
+    setDataOfOrders(data);
+    setLoading(false);
+  }
+}, [data]);
 
   const handleDelete = async (id_orderProduct) => {
     try {
@@ -209,6 +224,7 @@ const handleSuivre =  () => {
     <div className="max-w-6xl mx-auto p-6">
       <h1 className="text-2xl font-bold mb-6">Votre panier</h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        
         {/* Produits */}
         <div className="lg:col-span-2 bg-white rounded-2xl shadow p-4 space-y-6">
   {
